b0eaa63e435f1d54ed5b8114ccb7afdd12ed9760
Monica  Karasek <mlkarasek@gmail.com>
2017-10-19 23:46:28 +0000

    automatically backed up by learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -19,5 +19,5 @@ end
 def adding
   shipping_manifest = {"whale bone corsets" => 5, "porcelain vases" => 2, "oil paintings" => 3}
   shipping_manifest["pearl necklace"] = "1"
-  puts shipping_manifest
+  shipping_manifest
 end
